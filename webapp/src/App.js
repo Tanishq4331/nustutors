8eb432721a1c80e84c3b355bcfe1375d98343bf4
Tanishq4331 <tanishq43311@gmail.com>
2021-06-22 00:08:01 +0800

    fix commit

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,12 +3,14 @@ import "@firebase/firestore";
 import Body from "./components/Body";
 import "./styles.css";
 import { AuthProvider } from "./contexts/AuthContext";
+import { NavDropdown, Nav, Navbar } from "react-bootstrap";
+
 import React from "react";
 
 export default function App() {
   return (
     <AuthProvider>
-        <Body />
+      <Body />
     </AuthProvider>
   );
 }
---
+++ webapp/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function Home() {
+  return (
+    <>
+      <h1> Home </h1>
+      <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">click here</a>
+    </>
+  );
+}
